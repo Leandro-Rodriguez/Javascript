2130a037ab386c3f2c2a161c00a6b6b77b4742f3
Leandro Rodriguez <leandroricardorodriguez@gmail.com>
2024-04-11 10:46:05 -0300

    Emprolijamiento

--- func.js
+++ func.js
@@ -1,4 +1,5 @@
 let entradasDisponibles = 100;
+
 function venderEntradas() {
     let cantidad = parseInt(document.getElementById("cantidadEntradas").value);
     if (cantidad <= entradasDisponibles && cantidad > 0) {
@@ -7,11 +8,17 @@ function venderEntradas() {
         console.log(`Se vendieron ${cantidad} entradas. Quedan ${entradasDisponibles} entradas disponibles.`);
     } else {
         if (cantidad < 0) {
-            console.log("Lo siento, la cantidad no puede ser menor a cero.");
-            alert("Lo siento, la cantidad no puede ser menor a cero.");
+            console.log("La cantidad no puede ser menor a cero.");
+            alert("La cantidad no puede ser menor a cero.");
         } else {
-            console.log("Lo siento, no hay suficientes entradas disponibles.");
-            alert("Lo siento, no hay suficientes entradas disponibles.");
+            console.log("Sorry no hay esa cantidad de entradas disponibles.");
+            alert("Sorry no hay esa cantidad de entradas disponibles.");
         }
+    }
+
+    // Ciclo while para verificar si todavía hay entradas disponibles
+    while (entradasDisponibles > 0) {
+        document.getElementById("stockMessage").style.display = "block"; // Muestra el mensaje de stock
+        break; // Se sale del ciclo para evitar un bucle infinito
     }
 }
