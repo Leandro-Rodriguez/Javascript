1bd08550f0067b607e412d8b9c9dfba79fdfeee4
Leandro Rodriguez <leandroricardorodriguez@gmail.com>
2024-04-23 02:02:38 -0300

    Primera PreEntrega 1 Edit

--- func.js
+++ func.js
@@ -6,16 +6,18 @@ function venderEntradas() {
     if (cantidad <= entradasDisponibles && cantidad > 0) {
         entradasDisponibles -= cantidad;
         document.getElementById("entradasDisponibles").textContent = entradasDisponibles;
-        console.log(`Se vendieron ${cantidad} entradas. Quedan ${entradasDisponibles} entradas disponibles.`);
+        console.log(`Compraste ${cantidad} entraduki/s. Quedan ${entradasDisponibles} entradas disponibles.`);
+        alert(`Compraste ${cantidad} entraduki/s. Quedan ${entradasDisponibles} entradas disponibles.`);
     } else {
-        if (cantidad < 0) {
-            console.log("La cantidad no puede ser menor a cero.");
-            alert("La cantidad no puede ser menor a cero.");
+        if (cantidad <= 0) {
+            console.log("La cantidad no puede ser menor o igual a cero bro.");
+            alert("La cantidad no puede ser menor o igual a cero bro.");
         } else {
-            console.log("Sorry no hay esa cantidad de entradas disponibles.");
-            alert("Sorry no hay esa cantidad de entradas disponibles.");
+            console.log(`Sorry no tenemos esa cantidad. Quedan ${entradasDisponibles} entradas disponibles.`);
+            alert(`Sorry no tenemos esa cantidad. Quedan ${entradasDisponibles} entradas disponibles.`);
         }
     }
+    
 
     // While para verificar si todavía hay entradas disponibles
     while (entradasDisponibles > 0) {
